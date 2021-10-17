77ffbc68b7593f4186cd04d88e00f607db25ea91
Paima Marbun <paima.marbun@me.com>
2021-10-17 15:36:26 +0200

    use for other test purpose

--- test_path_finding.py
+++ test_path_finding.py
@@ -10,21 +10,26 @@ import numpy as np
 import itertools
 
 
-testmaze = Maze('test_maze_01.txt')
+testmaze = Maze('test_maze_08.txt')
 testrobot = Robot(testmaze.dim)
+
+mazeanim = MazeAnimation(testmaze,[0,0],"up", 60)
+mazeanim.showmaze()
+
+mazeanim.window.exitonclick()
 
 #print("walls from Maze", testmaze.walls)
 
-testrobot.maps[testrobot.Page.walls]= testmaze.walls
+#testrobot.maps[testrobot.Page.walls]= testmaze.walls
 
 #print("walls from robot", testrobot.maps[testrobot.Page.walls])
 
 
-maze_center = [(testmaze.dim/ 2) - 1, testmaze.dim / 2]
+#maze_center = [(testmaze.dim/ 2) - 1, testmaze.dim / 2]
 
-goals = [[product[0], product[1]] for product in itertools.product(maze_center, repeat=2)]
+#goals = [[product[0], product[1]] for product in itertools.product(maze_center, repeat=2)]
 
-result = testrobot.path_finding([0,0], "u", goals )
+#result = testrobot.path_finding([0,0], "u", goals )
 
-print("results:", result)
+#print("results:", result)
 
